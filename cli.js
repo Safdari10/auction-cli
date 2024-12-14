741416d5f3864ed045c0557388e068dfcea3ac03
Zaker Safdari <zakersafdari@gmail.com>
2024-12-14 23:14:47 +1300

    fixed import of by importing { Command } directly from the commander package.

--- cli.js
+++ cli.js
@@ -1,37 +1,37 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 
-const Command = require('commander');
-const { seeDataIntoDB, deleteDataFromDB } = require("./seed") 
+const { Command } = require("commander");
+const { seeDataIntoDB, deleteDataFromDB } = require("./seed");
 
 const program = new Command();
 
 program
-    .name( "Auction CLI" )
-    .description( "CLI tool to manage auction items in MongoDB" )
-    .version( "1.0.0" )
+  .name("Auction CLI")
+  .description("CLI tool to manage auction items in MongoDB")
+  .version("1.0.0");
 
 program
-    .command( "seed" )
-    .description( "Seed data into MongoDB" )
-    .action(async () => {
-        try {
-            await seeDataIntoDB()
-            console.log("Seed operation completed successfully")
-        } catch (error) {
-            console.error("Error during seed operation", error)
-        } 
-    })
+  .command("seed")
+  .description("Seed data into MongoDB")
+  .action(async () => {
+    try {
+      await seeDataIntoDB();
+      console.log("Seed operation completed successfully");
+    } catch (error) {
+      console.error("Error during seed operation", error);
+    }
+  });
 
 program
-       .command( "delete" )
-        .description( "Delete data from MongoDB" )
-        .action(async () => {
-            try {
-                await deleteDataFromDB()
-                console.log("Delete operation completed successfully")
-            } catch (error) {
-                console.error("Error during delete operation", error)
-            } 
-        })    
+  .command("delete")
+  .description("Delete data from MongoDB")
+  .action(async () => {
+    try {
+      await deleteDataFromDB();
+      console.log("Delete operation completed successfully");
+    } catch (error) {
+      console.error("Error during delete operation", error);
+    }
+  });
 
-program.parse( process.argv )
+program.parse(process.argv);
