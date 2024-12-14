313c652eb38c1599af40d49831274da0c3b3aa63
Zaker Safdari <zakersafdari@gmail.com>
2024-12-14 22:22:10 +1300

    Refactor seed.js to improve code formatting and export deleteDataFromDB function

--- seed.js
+++ seed.js
@@ -1,46 +1,43 @@
-const mongoose = require('mongoose');
-const AuctionItem = require('./models/auctionItem');
-const seedData = require('./seedData');
+const mongoose = require("mongoose");
+const AuctionItem = require("./models/auctionItem");
+const seedData = require("./seedData");
 
- // connection URI
-    const uri = 'mongodb://localhost:27017/auction';
+// connection URI
+const uri = "mongodb://localhost:27017/auction";
 
 // Seed data into MongoDB
 const seedDataIntoDB = async () => {
-    try {
-     await mongoose.connect(uri)
-     console.log("Connected to MongoDB")
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
 
-     await AuctionItem.deleteMany({})
-     console.log("Cleared existing auction items")
+    await AuctionItem.deleteMany({});
+    console.log("Cleared existing auction items");
 
-     await AuctionItem.insertMany(seedData)
-     console.log("Seed data inserted scuccessfully")
-
-    } catch (error) {
-        console.error("Error seeding data:", error)
-    } finally {
-        await mongoose.disconnect()
-        console.log("Disconnected from MongoDB")
-    }
-}
+    await AuctionItem.insertMany(seedData);
+    console.log("Seed data inserted scuccessfully");
+  } catch (error) {
+    console.error("Error seeding data:", error);
+  } finally {
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB");
+  }
+};
 
 // delete data from MongoDB
 const deleteDataFromDB = async () => {
-    try {
-        await mongoose.connect(uri)
-        console.log("Connected to MongoDB")
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
 
-        await AuctionItem.deleteMany({})
-        console.log("All auction items deleted")
+    await AuctionItem.deleteMany({});
+    console.log("All auction items deleted");
+  } catch (error) {
+    console.error("Error deleting data:", error);
+  } finally {
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB");
+  }
+};
 
-    } catch (error) {
-        console.error("Error deleting data:", error)
-    } finally {
-        await mongoose.disconnect()
-        console.log("Disconnected from MongoDB")
-    }
-}
-
-
-module.exports = seedDataIntoDB;
+module.exports = { seedDataIntoDB, deleteDataFromDB };
