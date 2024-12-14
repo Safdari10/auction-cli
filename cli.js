f83e74f0d20e52d6e147fecc322ece456a35444e
Zaker Safdari <zakersafdari@gmail.com>
2024-12-14 22:38:37 +1300

    Enhance CLI setup in cli.js with program name, description, and version

--- cli.js
+++ cli.js
@@ -1,3 +1,9 @@
 const Command = require('commander');
+const { seeDataIntoDB, deleteDataFromDB } = require("./seed") 
 
 const program = new Command();
+
+program
+    .name( "Auction CLI" )
+    .description( "CLI tool to manage auction items in MongoDB" )
+    .version( "1.0.0" )
