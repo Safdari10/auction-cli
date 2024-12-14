9644bf38a2c69ffd75a34561001fcfe57ef220c1
Zaker Safdari <zakersafdari@gmail.com>
2024-12-14 21:57:43 +1300

    Defined and exported auction item schema in auctionItem.js

--- models/auctionItem.js
+++ models/auctionItem.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+
+// schema for auction items
+const auctionItemSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    start_price: {type: Number, required: true},
+    reserve_price: {type: Number, required: true},
+})
+
+module.exports = mongoose.model('AuctionItem', auctionItemSchema);
