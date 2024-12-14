b7924b09329fc2986bfd85db079343a6fdb2c6db
Zaker Safdari <zakersafdari@gmail.com>
2024-12-14 21:52:10 +1300

    Added MongoDB connection function to index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');
 
+// Connect to MongoDB
+const connectDB = async () => {
+    // connection URI
+    const uri = 'mongodb://localhost:27017/auction';
 
+    try {
+        await mongoose.connect(uri)
+        console.log('MongoDB Connected...')
+    } catch (err) {
+        console.error(err.message)
+        process.exit(1)
+    }
+
+}
